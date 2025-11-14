f5fd0854fd8031c94cbc581f945f309398e5e749
Sourabh <kumarsourabh1109@gmail.com>
2025-11-14 11:22:56 +0530

    first

---
+++ src/components/Square.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Square() {
+  return (
+    <div>
+      <button className='square'>
+        Square
+      </button>
+    </div>
+  )
+}
+
+
+
+
